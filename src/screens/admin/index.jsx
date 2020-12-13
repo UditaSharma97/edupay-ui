3d4c976c0a8ef061845dc903bbe1d468da85bfac
uditaSharma97 <udiis97@gmail.com>
2020-12-13 06:32:18 +0530

    adminHomePage dummy changes

--- src/screens/admin/index.jsx
+++ src/screens/admin/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../App.css";
-import loginImg from "../../login.svg";
+import loginImg from "../../logoImg.png";
 import { INDEX_PATH } from "../../constants/pageNames";
 
 export class AdminPage extends React.Component{
@@ -12,14 +12,22 @@ export class AdminPage extends React.Component{
     render(){
         return(
 
-            <div className="admin-top-left-container">
-                <div className="logo">
-                    <img src={loginImg}/>
+            <div className="admin-top-container" style={{ display: "flex",flexDirection: "rows" }}>
+                <div className="logo" style={{width: "10%",height: "10%",objectFit: "contain"}}>
+                    <img src={loginImg}  
+                    style={{width: "100%",height: "100%",objectFit: "contain",paddingLeft: "15%",paddingRight:"15%" }}
+                    />
                 </div>
-                <div className="admin-top-middle-container">
+                <div className="admin-top-middle-container"
+                    style={{ width:"80%",textAlign:"center",paddingTop:"20px" }}
+                    >
                     Powered By EduPay
                 </div>
-                <div className="admin-top-right-container">
+                
+                <div className="admin-top-right-container"
+                style={{paddingTop:"7px",paddingLeft: "2px",paddingRight:"2px" }}
+                    
+                >
                     <button className="btn"  onClick={()=>{this.props.history.push(INDEX_PATH)}}>
                         Logout
                     </button>
